a94b268b9eb26ba89e051ee8b8ccb5fbd7153458
sahilofficial97 <91385151+sahilofficial97@users.noreply.github.com>
2023-03-09 14:58:37 +0100

    FEAT: adding security configuration to back-end

---
+++ meFit/src/main/java/com/noroff/mefit/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.noroff.mefit.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.cors().and().sessionManagement().disable().csrf().disable()
+                .authorizeHttpRequests(auth-> auth
+                        .anyRequest().authenticated()
+                        .requestMatchers("/permit/all").permitAll()
+                        .requestMatchers("/api/something").hasRole("ADMIN")
+                ).oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter(){
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter=new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter=new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+
+}
